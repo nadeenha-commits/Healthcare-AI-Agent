10269dede869d7edbe4793ee11dcf746aae02781
nadeenha <nadeenha@LAB1_07>
2026-06-21 15:38:22 +0300

    d

--- frontend/src/components/Chat.tsx
+++ frontend/src/components/Chat.tsx
@@ -13,6 +13,7 @@ interface Message {
   }>;
 }
 
+
 export const Chat: React.FC = () => {
   const [messages, setMessages] = useState<Message[]>([]);
   const [inputValue, setInputValue] = useState('');
